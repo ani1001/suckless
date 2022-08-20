4d08cffe498f731d0eee71ebb5160b6ead7d5b69
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2022-08-20 22:22:44 +0600

    Add files via upload

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:pixelsize=14:antialias=true:autohint=true";
+static char *font = "SauceCodePro NF:pixelsize=15:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
